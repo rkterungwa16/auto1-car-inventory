38c5c203da8a1c2ebcbf74551405c4a8efa9a409
richard_kombol <richard@ashnelsonpartners.com>
2018-11-03 13:08:03 +0100

    Implement components for filters container

---
+++ src/components/CarFiltersArea.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CarFiltersArea = () => (
+  <div className='filters-container'>
+    <div className='filter__dropdown'>
+      First dropdown
+    </div>
+    <div className='filter__dropdown'>
+      Second dropdown
+    </div>
+  </div>
+)
+
+export default CarFiltersArea
